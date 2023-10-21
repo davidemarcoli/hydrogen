b7a56f19e709189cef5aa24681c3d93ed88dd7fa
davidemarcoli <davide@marcoli.ch>
2023-10-21 16:27:26 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <vector>
+
+#include "tokenization.hpp"
+#include "parser.hpp"
+#include "generation.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
+        std::cerr << "hydro <input.hy>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    Tokenizer tokenizer(std::move(contents));
+    std::vector<Token> tokens = tokenizer.tokenize();
+
+    Parser parser(std::move(tokens));
+    std::optional<NodeProg> prog = parser.parse_prog();
+
+    if (!prog.has_value()) {
+        std::cerr << "Invalid program" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    Generator generator(prog.value());
+
+    {
+        std::fstream file("out.asm", std::ios::out);
+        file << generator.gen_prog();
+    }
+
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
+
+    return EXIT_SUCCESS;
+}
---
+++ src/tokenization.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+enum class TokenType {
+    exit,
+    int_lit,
+    semi,
+    open_paren,
+    close_paren,
+    ident,
+    let,
+    eq
+};
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value{};
+};
+
+class Tokenizer {
+public:
+    inline explicit Tokenizer(std::string src)
+            : m_src(std::move(src)) {
+    }
+
+    inline std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        std::string buf;
+
+        while (peek().has_value()) {
+            if (std::isalpha(peek().value())) {
+                buf.push_back(consume());
+                while (peek().has_value() && std::isalpha(peek().value())) {
+                    buf.push_back(consume());
+                }
+
+                if (buf == "exit") {
+                    tokens.push_back({.type = TokenType::exit});
+                    buf.clear();
+                    continue;
+                } else if (buf == "let") {
+                    tokens.push_back({.type = TokenType::let});
+                    buf.clear();
+                    continue;
+                } else {
+                    tokens.push_back({.type = TokenType::ident, .value = buf});
+                    buf.clear();
+                    continue;
+                }
+            } else if (std::isdigit(peek().value())) {
+                buf.push_back(consume());
+                while (peek().has_value() && std::isdigit(peek().value())) {
+                    buf.push_back(consume());
+                }
+                tokens.push_back({.type = TokenType::int_lit, .value = buf});
+                buf.clear();
+                continue;
+            } else if (peek().value() == '(') {
+                consume();
+                tokens.push_back({.type = TokenType::open_paren});
+                continue;
+            } else if (peek().value() == ')') {
+                consume();
+                tokens.push_back({.type = TokenType::close_paren});
+                continue;
+            } else if (peek().value() == ';') {
+                consume();
+                tokens.push_back({.type = TokenType::semi});
+                continue;
+            } else if (peek().value() == '=') {
+                consume();
+                tokens.push_back({.type = TokenType::eq});
+                continue;
+            } else if (isspace(peek().value())) {
+                consume();
+                continue;
+            } else {
+                std::cerr << "You messed up!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        m_index = 0;
+        return tokens;
+    }
+
+private:
+
+    [[nodiscard]] inline std::optional<char> peek(int offset = 0) const {
+        if (m_index + offset >= m_src.length()) {
+            return {};
+        } else {
+            return m_src.at(m_index + offset);
+        }
+    }
+
+    inline char consume() {
+        return m_src.at(m_index++);
+    }
+
+    const std::string m_src;
+    size_t m_index = 0;
+};
